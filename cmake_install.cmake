78ae9cdd1befd249a8aaa06ef495a6003859d0ac
🫠 <8655881+emp7y@user.noreply.gitee.com>
2026-01-06 20:35:29 +0800

    fix:File corruption

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lxw/文档/range scan v2注释
+# Install script for directory: /Users/linxinwei/Downloads/range scan/RIOKV
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,77 +27,100 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/home/lxw/文档/range scan v2注释/libleveldb.a")
+# Set path to fallback-tool for dependency-resolution.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "/Users/linxinwei/Downloads/range scan/RIOKV/libleveldb.a")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libleveldb.a" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libleveldb.a")
+    execute_process(COMMAND "/usr/bin/ranlib" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libleveldb.a")
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/leveldb" TYPE FILE FILES
-    "/home/lxw/文档/range scan v2注释/include/leveldb/c.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/cache.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/comparator.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/db.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/dumpfile.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/env.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/export.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/filter_policy.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/iterator.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/options.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/slice.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/status.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/table_builder.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/table.h"
-    "/home/lxw/文档/range scan v2注释/include/leveldb/write_batch.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/c.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/cache.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/comparator.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/db.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/dumpfile.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/env.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/export.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/filter_policy.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/iterator.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/options.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/slice.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/status.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/table_builder.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/table.h"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/include/leveldb/write_batch.h"
     )
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb/leveldbTargets.cmake")
-    file(DIFFERENT EXPORT_FILE_CHANGED FILES
+    file(DIFFERENT _cmake_export_file_changed FILES
          "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb/leveldbTargets.cmake"
-         "/home/lxw/文档/range scan v2注释/CMakeFiles/Export/lib/cmake/leveldb/leveldbTargets.cmake")
-    if(EXPORT_FILE_CHANGED)
-      file(GLOB OLD_CONFIG_FILES "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb/leveldbTargets-*.cmake")
-      if(OLD_CONFIG_FILES)
-        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb/leveldbTargets.cmake\" will be replaced.  Removing files [${OLD_CONFIG_FILES}].")
-        file(REMOVE ${OLD_CONFIG_FILES})
+         "/Users/linxinwei/Downloads/range scan/RIOKV/CMakeFiles/Export/f90a79f6c24c38ae6b0a9cccec147da8/leveldbTargets.cmake")
+    if(_cmake_export_file_changed)
+      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb/leveldbTargets-*.cmake")
+      if(_cmake_old_config_files)
+        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
+        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb/leveldbTargets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
+        unset(_cmake_old_config_files_text)
+        file(REMOVE ${_cmake_old_config_files})
       endif()
+      unset(_cmake_old_config_files)
     endif()
+    unset(_cmake_export_file_changed)
   endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb" TYPE FILE FILES "/home/lxw/文档/range scan v2注释/CMakeFiles/Export/lib/cmake/leveldb/leveldbTargets.cmake")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb" TYPE FILE FILES "/Users/linxinwei/Downloads/range scan/RIOKV/CMakeFiles/Export/f90a79f6c24c38ae6b0a9cccec147da8/leveldbTargets.cmake")
+  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^()$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb" TYPE FILE FILES "/Users/linxinwei/Downloads/range scan/RIOKV/CMakeFiles/Export/f90a79f6c24c38ae6b0a9cccec147da8/leveldbTargets-noconfig.cmake")
+  endif()
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/leveldb" TYPE FILE FILES
-    "/home/lxw/文档/range scan v2注释/cmake/leveldbConfig.cmake"
-    "/home/lxw/文档/range scan v2注释/cmake/leveldbConfigVersion.cmake"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/cmake/leveldbConfig.cmake"
+    "/Users/linxinwei/Downloads/range scan/RIOKV/cmake/leveldbConfigVersion.cmake"
     )
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/lxw/文档/range scan v2注释/third_party/googletest/cmake_install.cmake")
-  include("/home/lxw/文档/range scan v2注释/third_party/benchmark/cmake_install.cmake")
+  include("/Users/linxinwei/Downloads/range scan/RIOKV/third_party/googletest/cmake_install.cmake")
+  include("/Users/linxinwei/Downloads/range scan/RIOKV/third_party/benchmark/cmake_install.cmake")
 
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lxw/文档/range scan v2注释/${CMAKE_INSTALL_MANIFEST}"
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/Users/linxinwei/Downloads/range scan/RIOKV/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
+if(CMAKE_INSTALL_COMPONENT)
+  if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
+    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+  else()
+    string(MD5 CMAKE_INST_COMP_HASH "${CMAKE_INSTALL_COMPONENT}")
+    set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INST_COMP_HASH}.txt")
+    unset(CMAKE_INST_COMP_HASH)
+  endif()
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/Users/linxinwei/Downloads/range scan/RIOKV/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
--- include/port/port_config.h
+++ include/port/port_config.h
@@ -7,12 +7,12 @@
 
 // Define to 1 if you have a definition for fdatasync() in <unistd.h>.
 #if !defined(HAVE_FDATASYNC)
-#define HAVE_FDATASYNC 1
+#define HAVE_FDATASYNC 0
 #endif  // !defined(HAVE_FDATASYNC)
 
 // Define to 1 if you have a definition for F_FULLFSYNC in <fcntl.h>.
 #if !defined(HAVE_FULLFSYNC)
-#define HAVE_FULLFSYNC 0
+#define HAVE_FULLFSYNC 1
 #endif  // !defined(HAVE_FULLFSYNC)
 
 // Define to 1 if you have a definition for O_CLOEXEC in <fcntl.h>.
@@ -27,7 +27,7 @@
 
 // Define to 1 if you have Google Snappy.
 #if !defined(HAVE_SNAPPY)
-#define HAVE_SNAPPY 1
+#define HAVE_SNAPPY 0
 #endif  // !defined(HAVE_SNAPPY)
 
 // Define to 1 if you have Zstd.
